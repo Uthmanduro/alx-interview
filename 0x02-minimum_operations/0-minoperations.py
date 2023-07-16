e9992282ba4f39c446b2faeca026b83728abdeac
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-07-16 15:10:06 -0700

    min op

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,9 +3,27 @@
 
 
 def minOperations(n):
-    """defines the function minperation"""
-    arr = []
-    for i in range(1, n + 1):
-        if n % i == 0:
-            arr.append(i)
-    return len(arr) + 1
+    """returns an integer"""
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 1
+    arr = ["h"]
+    exp_result = "h" * n
+    cp = arr[0]
+    paste = cp
+    count = 1
+    while True:
+        paste += cp
+        arr.append(paste)
+        count += 1
+        if arr[-1] == exp_result:
+            return count
+        if len(exp_result) % len(arr[-1]) == 0:
+            cp = arr[-1]
+            count += 1
+            paste += cp
+            arr.append(paste)
+            count += 1
+            if arr[-1] == exp_result:
+                return count
