7ed260074fbe47dd9558efbd6c82014d8c54f572
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-07-16 18:33:39 -0700

    done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,29 +1,10 @@
 #!/usr/bin/python3
 """return the number of minimum operations"""
-import math
 
-
-def is_prime(n):
-    if n <= 1:
-        return False
-    if n == 2 or n == 3:
-        return True
-    if n % 2 == 0:
-        return False
-
-    sqrt_n = int(math.sqrt(n)) + 1
-    for i in range(3, sqrt_n, 2):
-        if n % i == 0:
-            return False
-
-    return True
 
 def minOperations(n):
     """returns an integer"""
     if n <= 0 or n == 1:
-        return 0
-
-    if is_prime(n):
         return 0
 
     arr = ["h"]
