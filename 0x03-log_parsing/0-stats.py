97ef288c6dbcd5d900f7a366dc12e0e0cafa5eda
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-07-23 05:12:31 -0700

    try

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,8 +7,8 @@ count = 0
 codes = {'200': 0, '301': 0, '400': 0, '401': 0,
          '403': 0, '404': 0, '405': 0, '500': 0}
 file_size = 0
-for line in sys.stdin:
-    try:
+try:
+    for line in sys.stdin:
         count += 1
         args = line.split()
         file_size += int(args[-1])
@@ -20,10 +20,14 @@ for line in sys.stdin:
                 if value == 0 or not str(value).isdigit():
                     continue
                 print("{}: {}".format(key, value))
-    except KeyboardInterrupt:
-        print("File size: {}".format(file_size))
-        for key, value in codes.items():
-            if value == 0 or not str(value).isdigit():
-                continue
-            print("{}: {}".format(key, value))
-        break
+    print("File size: {}".format(file_size))
+    for key, value in codes.items():
+        if value == 0 or not str(value).isdigit():
+            continue
+        print("{}: {}".format(key, value))
+except KeyboardInterrupt:
+    print("File size: {}".format(file_size))
+    for key, value in codes.items():
+        if value == 0 or not str(value).isdigit():
+            continue
+        print("{}: {}".format(key, value))
