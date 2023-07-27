5b8bb9076d65a080b764ddf067537ac1bee1d1f3
uthmanduro <udurosinlohun@gmail.com>
2023-07-27 17:10:37 +0100

    done

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,18 +7,24 @@ def validUTF8(data):
     if not isinstance(data, list):
         return False
     for item in data:
+        if not all([isinstance(item, int)]):
+            return False
+    for item in data:
         binary = bin(item)[2:]
         second_byte = binary[8:16]
+        third_byte = binary[16:24]
+        fourth_byte = binary[24:32]
         if len(binary) <= 8 or binary[:1] == "0":
             continue
         elif len(binary) > 8 and len(binary) <= 16 and binary[:3] == "110"\
                 and second_byte[:2] == "10":
             continue
         elif len(binary) > 16 and len(binary) <= 24 and binary[:4] == "1110"\
-                and second_byte[:2] == "10":
+                and second_byte[:2] == "10" and third_byte[:2] == "10":
             continue
         elif len(binary) > 24 and len(binary) <= 32 and binary[:5] == "11110"\
-                and second_byte[:2] == "10":
+                and second_byte[:2] == "10" and third_byte[:2] == "10"\
+                and fourth_byte[:2] == "10":
             continue
         else:
             return False
