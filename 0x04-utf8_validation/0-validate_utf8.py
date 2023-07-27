a8d73abd0c4698cdecdf168807d523146f7f7596
uthmanduro <udurosinlohun@gmail.com>
2023-07-27 23:11:13 +0100

    fixed LSB

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,39 +11,16 @@ def validUTF8(data):
     except Exception:
         return False
     for item in data:  # check if item is an int
-        binary = bin(item)[2:]
-        if len(binary) > 0 and len(binary) <= 8:  # check if item is 1 byte
-            binary = binary.zfill(8)  # fill with 0s to make 8 bits
-            if binary.startswith("0"):  # check if item is 1 byte
-                continue
-            else:
-                return False
-        elif len(binary) > 8 and len(binary) <= 16:  # check if item is 2 bytes
-            sec_byte = binary[8:16]
-            if binary.startswith("110") and sec_byte.startswith("10"):
-                # check first 3 bits and 2nd byte
-                continue
-            else:
-                return False
-        elif len(binary) > 16 and len(binary) <= 24:
-            # check if item is 3 bytes
-            sec_byte = binary[8:16]
-            thd_byte = binary[16:24]
-            if binary.startswith("1110") and sec_byte.startswith("10") and\
-                    thd_byte.startswith("10"):
-                #  check first 4 bits, 2nd byte and 3rd byte
-                continue
-            else:
-                return False
-        elif len(binary) > 24 and len(binary) <= 32:
-            # check if item is 4 bytes
-            sec_byte = binary[8:16]  # extract 2nd byte
-            thd_byte = binary[16:24]   # extract 3rd byte
-            foth_byte = binary[24:32]  # extract 4th byte
-            if binary.startswith("11110") and sec_byte.startswith("10")\
-                    and thd_byte.startswith("10")\
-                    and foth_byte.startswith("10"):
-                continue
-            else:
-                return False
+        binary = item & 0xFF
+        binary = bin(binary)[2:].zfill(8)  # convert to binary
+        if binary.startswith("0"):  # check if item is 1 byte
+            continue
+        elif binary.startswith("110") and binary[4:8].startswith("10"):
+            continue
+        elif binary.startswith("1110") and binary[4:8].startswith("10"):
+            continue
+        elif binary.startswith("11110") and binary[4:8].startswith("10"):
+            continue
+        else:
+            return False
     return True
