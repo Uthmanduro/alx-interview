4a199896f5b0e8c0ce0d730d7d5fe878e7fe3f7a
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-08-24 14:27:51 -0700

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Change comes from within"""
+
+
+def make_change(coins, total):
+    """returns fewest number of coins needed \
+        to meet a given amount total"""
+    if total <= 0:
+        return 0
+
+    init_total = 0
+    used_coins = 0
+    coins = sorted(coins, reverse=True)
+    for coin in coins:
+        solution = (total - init_total) // coin
+        init_total += solution * coin
+        used_coins += solution
+        if init_total == total:
+            return used_coins
+    return -1
